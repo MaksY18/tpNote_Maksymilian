5b87fe84dd08947683f7fa9a62623123e24ae35a
Maksymilian <makspomo8@gmail.com>
2024-12-20 18:16:40 +0100

    Correction des fixtures et ajout de l'index

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\TopicRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class DefaultController extends AbstractController
 {
-    #[Route('/default', name: 'app_default')]
-    public function index(): Response
+    #[Route('/', name: 'app_home')]
+    public function index(TopicRepository $topicRepository): Response
     {
+        $topics = $topicRepository->findAll();
+
         return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
+            'topics' => $topics,
         ]);
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,14 +4,43 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Nelmio\Alice\Loader\NativeLoader;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // Initialiser le loader d'Alice
+        $loader = new NativeLoader();
 
-        $manager->flush();
+        // Charger les fichiers YAML dans l'ordre approprié
+        $categoryFixture = $loader->loadFile(__DIR__.'/../../fixtures/category.yaml');
+        $languageFixture = $loader->loadFile(__DIR__.'/../../fixtures/language.yaml');
+        $userFixture = $loader->loadFile(__DIR__.'/../../fixtures/user.yaml');
+        // $topicFixture = $loader->loadFile(__DIR__.'/../../fixtures/topic.yaml');
+
+        // Persister les catégories
+        foreach ($categoryFixture->getObjects() as $object) {
+            $manager->persist($object);
+        }
+        $manager->flush();  // Flush après catégories
+
+        // Persister les langues
+        foreach ($languageFixture->getObjects() as $object) {
+            $manager->persist($object);
+        }
+        $manager->flush();  // Flush après langues
+
+        // Persister les utilisateurs
+        foreach ($userFixture->getObjects() as $object) {
+            $manager->persist($object);
+        }
+        $manager->flush();  // Flush après utilisateurs
+
+        // Persister les topics
+        // foreach ($topicFixture->getObjects() as $object) {
+        //     $manager->persist($object);
+        // }
+        // $manager->flush();  // Flush après tous les objets
     }
 }
